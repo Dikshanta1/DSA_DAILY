3204d5b30a863c675bcb1cc9383ca9795ce06dfa
Dikshanta1 <aroradikshant3@gmail.com>
2026-02-02 09:48:55 +0530

    Feb1

---
+++ Solutions/February1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        sort(nums.begin()+1,nums.end());
+        return nums[0] + nums[1] + nums[2];
+    }
+};
