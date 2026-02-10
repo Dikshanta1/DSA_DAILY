c791e6a5884d74d70cfa1e1e7198076f450788c4
Dikshanta1 <aroradikshant3@gmail.com>
2026-02-11 01:47:18 +0530

    Feb 10 - Brute force only as per constraint

---
+++ Solutions/February10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int len = 0;
+        for(int i = 0 ; i < n ; i++){
+            unordered_map<int,int> even;
+            unordered_map<int,int> odd;
+            for(int j = i ; j < n ; j++){
+                if(nums[j] % 2 == 0) even[nums[j]]++;
+                else{odd[nums[j]]++;}
+
+                if(odd.size()== even.size()){
+                    len = max(len,j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+};
