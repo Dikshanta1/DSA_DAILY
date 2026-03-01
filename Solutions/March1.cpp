cf8e74d233b599f6e8d08f85562517e062e31560
Dikshanta1 <aroradikshant3@gmail.com>
2026-03-01 07:28:47 +0530

    March 1

---
+++ Solutions/March1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxi = 0;
+        for(auto x : n){
+            maxi = max(maxi,x-'0');
+        }
+        return maxi;
+    }
+};
